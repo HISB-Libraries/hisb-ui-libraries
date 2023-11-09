d6e70688eb090e0fc4248a2b8eb5364f1dfe1b99
azeezhus <ahussain63@gatech.edu>
2023-11-09 16:23:08 -0500

    added landing page buttons, logger service to testing comp

--- src/app/test-modules/hisb-logger-tester/components/logger-tester/logger-tester.component.ts
+++ src/app/test-modules/hisb-logger-tester/components/logger-tester/logger-tester.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {LogLine} from "ngx-hisb-logger";
+import {LoggerService} from "../../../../../../projects/ngx-hisb-logger/src/lib/services/logger.service";
 
 @Component({
   selector: 'app-logger-tester',
@@ -8,6 +9,7 @@ import {LogLine} from "ngx-hisb-logger";
 })
 export class LoggerTesterComponent {
 
+  constructor(private loggerService : LoggerService) {}
   loggerData: LogLine[];
 
 
