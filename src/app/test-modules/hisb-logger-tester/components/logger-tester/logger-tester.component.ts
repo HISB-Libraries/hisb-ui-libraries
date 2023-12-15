e15a0f95feaad30f34a821e847bba320cbedeb9e
Plamen Tassev <plamen@gatech.edu>
2023-12-15 17:13:39 -0500

    Updated the testing for the logger library

--- src/app/test-modules/hisb-logger-tester/components/logger-tester/logger-tester.component.ts
+++ src/app/test-modules/hisb-logger-tester/components/logger-tester/logger-tester.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LogLine, LoggerService} from "ngx-hisb-logger";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -7,13 +8,23 @@ import {LogLine, LoggerService} from "ngx-hisb-logger";
   templateUrl: './logger-tester.component.html',
   styleUrls: ['./logger-tester.component.scss']
 })
-export class LoggerTesterComponent {
-
-  constructor(private loggerService : LoggerService) {}
+export class LoggerTesterComponent implements OnInit{
+  form: FormGroup | undefined;
+  constructor(private loggerService : LoggerService, private formBuilder: FormBuilder) {}
   loggerData: LogLine[];
+  loggingLevelList = ['info', 'debug', 'warn', 'error'];
 
   ngOnInit(): void {
-    this.loggerService.logStream$.subscribe(value => this.loggerData = value);
+    this.loggerService.logStream$.subscribe(value => {
+      console.log(value);
+      this.loggerData = value;
+    });
+
+    this.form = this.formBuilder.group({
+      message: [null, [Validators.required]],
+      logLevel: [this.loggingLevelList[0]],
+      updateOn: 'submit'
+    });
   }
 
   onClear() {
@@ -21,19 +32,57 @@ export class LoggerTesterComponent {
   }
 
   onDebug() {
-    this.loggerService.debug('test debug msg', 'test debug src');
+    this.loggerService.debug('test debug msg', this.constructor.name);
   }
 
   onInfo() {
-    this.loggerService.info('test info msg', 'test info src');
+    this.loggerService.info('test info msg', this.constructor.name);
   }
 
   onWarn() {
-    this.loggerService.warn('test warn msg', 'test warn src');
+    this.loggerService.warn('test warn msg', this.constructor.name);
   }
 
   onError() {
-    this.loggerService.error('test error msg', 'test error src')
+    this.loggerService.error('test error msg', this.constructor.name);
   }
 
+  submit() {
+    if(!this.form.valid){
+      return;
+    }
+    const logMessage: string = this.form.controls['message'].value;
+
+    const logLevel: string = this.form.controls['logLevel'].value;
+
+    switch (logLevel){
+      case "info": {
+        this.loggerService.info(logMessage, this.constructor.name);
+        break;
+      }
+      case "debug": {
+        this.loggerService.debug(logMessage, this.constructor.name);
+        break;
+      }
+      case "warn": {
+        this.loggerService.warn(logMessage, this.constructor.name);
+        break;
+      }
+      case "error": {
+        this.loggerService.error(logMessage, this.constructor.name);
+        break;
+      }
+      default:
+        console.error(`Invalid Log Level ${logLevel}`)
+    }
+  }
+
+  onClearLog() {
+    this.loggerService.clear();
+  }
+
+  onClearFormData() {
+    this.form.reset();
+    this.form.controls['logLevel'].patchValue(this.loggingLevelList[0]);
+  }
 }
