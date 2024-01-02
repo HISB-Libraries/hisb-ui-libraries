4c46c45b5f9890749c7162ec2cd625c892ed6ec5
Plamen Tassev <plamen@gatech.edu>
2024-01-02 14:04:38 -0500

    Updated the testing for the validator library

--- src/app/test-modules/ngx-fhir-validator-tester/ngx-fhir-validator-tester.module.ts
+++ src/app/test-modules/ngx-fhir-validator-tester/ngx-fhir-validator-tester.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FhirValidatorTesterComponent } from './components/fhir-validator-tester/fhir-validator-tester.component';
 import {MatCardModule} from "@angular/material/card";
 import {NgxFhirValidatorModule} from "ngx-fhir-validator";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatRadioModule} from "@angular/material/radio";
 
 @NgModule({
   declarations: [
@@ -15,6 +20,12 @@ import {NgxFhirValidatorModule} from "ngx-fhir-validator";
     CommonModule,
     NgxFhirValidatorModule.forRoot("https://dev.heat.icl.gtri.org/fhir-validator-service/fhir"),
     MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatRadioModule,
   ]
 })
 export class NgxFhirValidatorTesterModule { }
