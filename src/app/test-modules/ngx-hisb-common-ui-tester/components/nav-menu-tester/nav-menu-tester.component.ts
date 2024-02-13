5d18d894b1d25a0a1807210827eee6b0d65a2df7
Plamen Tassev <plamen@gatech.edu>
2024-02-13 12:44:32 -0500

    Added nav menu testing components

--- src/app/test-modules/ngx-hisb-common-ui-tester/components/nav-menu-tester/nav-menu-tester.component.ts
+++ src/app/test-modules/ngx-hisb-common-ui-tester/components/nav-menu-tester/nav-menu-tester.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {NavMenuComponent} from "ngx-hisb-common-ui";
 @Component({
   selector: 'app-nav-menu-tester',
   templateUrl: './nav-menu-tester.component.html',
