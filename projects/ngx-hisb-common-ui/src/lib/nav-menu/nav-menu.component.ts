f269fab9dbb7b42d10e726759796f13c19ce49a3
Plamen Tassev <plamen@gatech.edu>
2024-09-16 17:56:08 -0400

    implemented initial selection for the nav menu

--- projects/ngx-hisb-common-ui/src/lib/nav-menu/nav-menu.component.ts
+++ projects/ngx-hisb-common-ui/src/lib/nav-menu/nav-menu.component.ts
@@ -1,12 +1,13 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {OptionConfig} from "./option.config";
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatTooltipModule} from "@angular/material/tooltip";
 
-import {ActivatedRoute, RouterLink} from "@angular/router";
+import {NavigationEnd, Router, RouterLink} from "@angular/router";
 import {MatMenuModule} from "@angular/material/menu";
+import {filter, take, tap} from "rxjs";
 
 
 @Component({
@@ -23,16 +24,19 @@ import {MatMenuModule} from "@angular/material/menu";
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent implements OnChanges {
+export class NavMenuComponent implements  AfterViewInit {
   @Input() backgroundColor: string = "#646064";
   @Input() contrastColor: string = "white";
   @Input() options: OptionConfig = {options: []};
-  @Input() currentOption: number = 0;
+  currentRoute: string; //We only use this variable to store the current route, it is set in the constructor and user in the ngAfterInit
 
   expanded: boolean = true;
   selectedOption = 0;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private router: Router) {
+    // To grab the router events immediately we need to subscribe to them in the constructor
+    this.setNavMenuInitialRoute();
+  }
 
   toggleSize() {
     this.expanded = !this.expanded;
@@ -42,11 +46,44 @@ export class NavMenuComponent implements OnChanges {
     this.selectedOption = i;
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if(changes['currentOption'].currentValue){
-      this.selectedOption = this.currentOption;
+  private setNavMenuInitialRoute(){
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      take(1),
+    ).subscribe(event => {
+      if(event?.['url']){
+        this.currentRoute = this.extractPath(event?.['url']);
+      }
+    })
+  }
+
+  /**
+   * We use this function to extract the current path from the current url
+   * @param inputString
+   * @private
+   */
+  private extractPath(inputString) {
+    const firstSlashIndex = inputString.indexOf('/');
+
+    if (firstSlashIndex === -1) {
+      return '';
+    }
+
+    const secondSlashIndex = inputString.indexOf('/', firstSlashIndex + 1);
+
+    if (secondSlashIndex === -1) {
+      return inputString.substring(firstSlashIndex + 1);
+    } else {
+      return inputString.substring(firstSlashIndex + 1, secondSlashIndex);
     }
   }
 
+  ngAfterViewInit(): void {
+    //Now that we have the options object, we select the current route using the this.currentRoute variable
+    const index = this.options.options.findIndex(option=> option.routerLink == this.currentRoute);
+    if(index >= 0 ){
+      this.selectedOption = index
+    }
+  }
 
 }
